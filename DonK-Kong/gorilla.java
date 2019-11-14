6703ecccc9d2531d2508ada6eab01dba8c4c82c5
佐藤 歩夢 <15083@kauth.local>
2019-11-14 09:33:40 +0900

    ゴリラクラス修正

---
+++ DonK-Kong/gorilla.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class gorilla here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class gorilla extends Actor
+{
+    /**
+     * Act - do whatever the gorilla wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public gorilla()
+    {
+        GreenfootImage image = getImage();
+        image.scale(image.getWidth()/2, image.getHeight()/2);
+        setImage(image);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
