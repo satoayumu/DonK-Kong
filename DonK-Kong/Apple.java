433d841ceec1c0acf85aa3269f0921a5df4d5d7a
Takotubo <teira32@yahoo.co.jp>
2019-12-06 10:34:30 +0900

    リンゴが転がる]

--- DonK-Kong/Apple.java
+++ DonK-Kong/Apple.java
@@ -8,12 +8,5 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Apple extends Thrown_object
 {
-    /**
-     * Act - do whatever the apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+    
 }
