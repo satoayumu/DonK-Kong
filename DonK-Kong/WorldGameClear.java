f4d18e742e6b294933a09a412fd4f9ae3ab98921
saki15048 <15048@hakodate.kosen-ac.jp>
2020-01-23 09:42:25 +0900

    score表示

--- DonK-Kong/WorldGameClear.java
+++ DonK-Kong/WorldGameClear.java
@@ -19,11 +19,17 @@ public class WorldGameClear extends World
         super(1200, 800, 1);  
     }
     public void act(){
+        int clear_time = MyWorld.time;
+        int life = Player.HP;
+        int anser = clear_time + life * 1000;
         GreenfootImage gg = new GreenfootImage("Game Clear", 200, Color.BLACK, new Color(0, 0, 0, 0));
         getBackground().drawImage(gg, (getWidth()-gg.getWidth())/2, 200);
         
+        GreenfootImage score = new GreenfootImage("score : " + String.valueOf(anser), 50, Color.BLACK, new Color(0, 0, 0, 0));
+        getBackground().drawImage(score, (getWidth()-score.getWidth())/2, 450);
+        
         GreenfootImage msg = new GreenfootImage("spaceでスタート画面へ", 50, Color.BLACK, new Color(0, 0, 0, 0));
-        getBackground().drawImage(msg, (getWidth()-msg.getWidth())/2, 450);
+        getBackground().drawImage(msg, (getWidth()-msg.getWidth())/2, 550);
 
         if( Greenfoot.isKeyDown( "space" ) ){
             World game = new WorldTitle();
