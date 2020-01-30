6035e1bd09bc17ff2d2aae7c2f7f71f4da1d28f5
Takotubo <teira32@yahoo.co.jp>
2020-01-30 10:23:56 +0900

    置きリンゴ

---
+++ DonK-Kong/Oki_Apple.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Oki_Apple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Oki_Apple extends Thrown_object
+{
+    /**
+     * Act - do whatever the Oki_Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Oki_Apple(){
+        super(null);
+        damage = 1;
+    }
+}
